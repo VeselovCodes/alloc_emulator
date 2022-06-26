46f9176ba694555cf492147041275701e64e9bdd
VeselovCodes <veselov.ilya.nn@gmail.com>
2022-06-26 13:04:41 -0300

    Add free method
    Added free method  for mem representation
    added additional internal checks
    small refactoring
    
    Signed-off-by: VeselovCodes <veselov.ilya.nn@gmail.com>

--- src/include/MemoryRepresentation/bitmap_representaation.hpp
+++ src/include/MemoryRepresentation/bitmap_representaation.hpp
@@ -9,32 +9,41 @@ public:
 	BitmapMemRepresentation() = delete;
 	BitmapMemRepresentation(size_t n) : mem_(n, false) {}
 
-	size_t getSize() { return mem_.size(); }
-	bool doesRangeFit(size_t offset, size_t size) {
+	size_t getSize() const { return mem_.size(); }
+	bool doesRangeFit(size_t offset, size_t size) const {
 	   	return offset + size < getSize();
    	}
-	bool isFree(size_t offset, size_t size) {
+	bool isFree(size_t offset, size_t size) const {
 	    return std::all_of(mem_.begin() + offset,
 						   mem_.begin() + offset + size - 1,
 						   [](bool el) { return !el; });
    	}
 	void alloc(size_t offset, size_t size) {
-		std::fill(mem_.begin() + offset,
-			      mem_.begin() + offset + std::min(getSize() - offset, size),
-				  true);
+		setMem(offset, size, true);
 	}
+	void free(size_t offset, size_t size) {
+		setMem(offset, size, false);
+	}
+
 	bool salloc(size_t offset,size_t size) {
 		if (!doesRangeFit(offset, size) || !isFree(offset, size)) return false;
 		alloc(offset, size);
 		return true;
 	}
-	void print() {
+	void print() const {
 		for (auto el : mem_) {
 			std::cout << el;
 		}
 		std::cout << std::endl;
 	}
 private:
+	void setMem(size_t offset, size_t size, bool value) {
+		if (offset >= getSize()) return;
+		std::fill(mem_.begin() + offset,
+			      mem_.begin() + offset + std::min(getSize() - offset, size),
+				  value);
+	}
+
 	std::vector<bool> mem_;
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main() {
 	auto bitmap_mem = BitmapMemRepresentation(10);
 	bitmap_mem.print();
-	bitmap_mem.alloc(2, 12);
+	bitmap_mem.alloc(10, 12);
 	std::cout << bitmap_mem.isFree(2, 4) << std::endl;
 	bitmap_mem.print();
 	return 0;
